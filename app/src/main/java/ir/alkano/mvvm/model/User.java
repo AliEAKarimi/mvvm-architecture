7d836afb48bba471186502ab0f263bfdbb5f3bde
AliEAKarimi <AliEAKarimi@gmail.com>
2021-01-18 23:44:21 +0330

    create User model class

--- app/src/main/java/ir/alkano/mvvm/model/User.java
+++ app/src/main/java/ir/alkano/mvvm/model/User.java
@@ -2,12 +2,19 @@ package ir.alkano.mvvm.model;
 
 public class User {
     private String name;
+    private String email;
 
     public void setName(String name) {
         this.name = name;
     }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     public String getName() {
         return name;
+    }
+    public String getEmail() {
+        return email;
     }
 }
