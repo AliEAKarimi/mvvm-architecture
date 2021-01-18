63253ef89abfdea2fb0af77686b376d80f0599d5
AliEAKarimi <AliEAKarimi@gmail.com>
2021-01-18 19:19:07 +0330

    start a new android project about MVVM architecture

---
+++ app/src/main/java/ir/alkano/mvvm/model/User.java
@@ -0,0 +1,13 @@
+package ir.alkano.mvvm.model;
+
+public class User {
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
