5702f6430ff69a5d841071f2772eb33c7690b847
AliEAKarimi <AliEAKarimi@gmail.com>
2021-01-19 23:50:04 +0330

    create recyclerview adapter

--- app/src/main/java/ir/alkano/mvvm/adapter/RecyclerViewAdapter.java
+++ app/src/main/java/ir/alkano/mvvm/adapter/RecyclerViewAdapter.java
@@ -1,37 +1,54 @@
 package ir.alkano.mvvm.adapter;
 
-import android.view.View;
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import ir.alkano.mvvm.R;
 import ir.alkano.mvvm.databinding.RecyclerviewItemBinding;
+import ir.alkano.mvvm.model.User;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+    private Context context;
+    private List<User> users;
+
+    public RecyclerViewAdapter(Context context, List<User> users) {
+        this.users = users;
+        this.context = context;
+    }
 
     @NonNull
     @Override
     public RecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        //View view = inflater.inflate(R.layout.recyclerview_item, parent, false);
+        RecyclerviewItemBinding binding = DataBindingUtil.inflate(inflater, R.layout.recyclerview_item, parent, false);
+        return new ViewHolder(binding);
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerViewAdapter.ViewHolder holder, int position) {
-
+        holder.binding.setUser(users.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return users.size();
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public  class ViewHolder extends RecyclerView.ViewHolder {
         private RecyclerviewItemBinding binding;
-
         public ViewHolder(@NonNull RecyclerviewItemBinding binding) {
             super(binding.getRoot());
+            this.binding = binding;
         }
     }
 }
