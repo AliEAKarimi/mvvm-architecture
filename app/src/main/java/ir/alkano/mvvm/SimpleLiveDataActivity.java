52abaeb380573f8b2a23b316092951b794b59fce
AliEAKarimi <AliEAKarimi@gmail.com>
2021-01-21 18:26:52 +0330

    new Activity and its layout named SimpleLiveData

---
+++ app/src/main/java/ir/alkano/mvvm/SimpleLiveDataActivity.java
@@ -0,0 +1,14 @@
+package ir.alkano.mvvm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SimpleLiveDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_live_data);
+    }
+}
